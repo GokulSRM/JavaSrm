de5b4bdda3983ae677217f6cfbbd8230b9e05a27
GokulSrm <gokulramaswamy@srmtech.com>
2022-05-08 11:34:49 +0530

    first commit

---
+++ examples/src/main/java/com/workout/examples/ExamplesApplication.java
@@ -0,0 +1,14 @@
+package com.workout.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplesApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ examples/src/test/java/com/workout/examples/ExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workout.examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
